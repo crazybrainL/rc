01ff68f4e4f69a19e185be449091663372dc17a2
whlin <whlin626@gmail.com>
2015-10-24 02:22:03 +0800

    update_rc.sh update

--- update_rc.sh
+++ update_rc.sh
@@ -1,6 +1,3 @@
-cp -r ./vim $RCPATH/vimfilers/vim
-cp ./vimrc $RCPATH/vimfilers/vimrc
-cp ./gvimrc $RCPATH/vimfilers/gvimrc
+cp ./gitconfig $RCPATH/rc/gitconfig
+cp ./screenrc $RCPATH/rc/screenrc
 
-#cp ./zshrc ~/rc_files/zshrc
-#cp ./whlin.zsh-theme ~/rc_files/whlin.zsh-theme
