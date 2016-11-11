457f92125510890131ef21de11af34437dc46a55
Wu-Hsuan Lin <wuhsuan_lin@trend.com.tw>
2016-11-11 15:48:31 +0800

    Update install_unix.sh

--- install_unix.sh
+++ install_unix.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
 ## Link dictionay and files
-rm ~/.screenrc | ln -s `pwd`/screenrc ~/.screenrc 
-rm ~/.gitconfig | ln -s `pwd`/gitconfig ~/.gitconfig
+PATHS=(`pwd`/screenrc `pwd`/gitconfig)
+LINKS=(~/.screenrc ~/.gitconfig)
+
+tLen=${#PATHS[@]}
+
+for (( i=0; i<${tLen}; i++ ));
+do
+    if [[ -e ${LINKS[$i]} ]]; then
+        echo "rm ${LINKS[$i]}"
+        rm ${LINKS[$i]}
+    else
+        echo "${LINKS[$i]} doesn't exist"
+    fi
+    echo "ln -s ${PATHS[$i]} ${LINKS[$i]}"
+    ln -s ${PATHS[$i]} ${LINKS[$i]}
+done
+
