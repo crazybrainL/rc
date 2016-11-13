f02dbfa29d2515c17008b380635b4ad412a236c3
Wu-Hsuan Lin <wuhsuan_lin@trend.com.tw>
2016-11-13 18:32:34 +0800

    Update install_unix for clean command.

--- install_unix.sh
+++ install_unix.sh
@@ -1,28 +1,48 @@
 #!/usr/bin/env bash
 
 ## Link dictionay and files
-RCHOME=~/Documents/rc
-
-if [[ ! -e $RCHOME ]]; then
-    git clone https://github.com/whlin/rc $RCHOME 
-fi
-
-PATHS=(`pwd`/screenrc `pwd`/gitconfig)
-LINKS=(~/.screenrc ~/.gitconfig)
-
+RCHOME=$HOME/Documents/rc
+CLEAN=false
 FILES=(screenrc gitconfig)
-
 tLen=${#FILES[@]}
 
-for (( i=0; i<${tLen}; i++ ));
+cleanRCFiles (){
+    for (( i=0; i<${tLen}; i++ ));
+    do
+        if [[ -e $HOME/.${FILES[$i]} ]]; then
+            echo "rm $HOME/.${FILES[$i]}"
+            rm $HOME/.${FILES[$i]}
+        else
+            echo "$HOME/.${FILES[$i]} doesn't exist"
+        fi
+    done
+}
+
+buildRCFiles (){
+    for (( i=0; i<${tLen}; i++ ));
+    do
+        echo "ln -s $RCHOME/${FILES[$i]} $HOME/.${FILES[$i]}"
+        ln -s $RCHOME/${FILES[$i]} $HOME/.${FILES[$i]}
+    done
+}
+
+while getopts c: opt
 do
-    if [[ -e $HOME/.${FILES[$i]} ]]; then
-        echo "rm $HOME/.${FILES[$i]}"
-        rm $HOME/.${FILES[$i]}
-    else
-        echo "$HOME/.${FILES[$i]} doesn't exist"
-    fi
-    echo "ln -s $RCHOME/${FILES[$i]} $HOME/.${FILES[$i]}"
-    ln -s $RCHOME/${FILES[$i]} $HOME/.${FILES[$i]}
+    case $opt in
+        c) CLEAN=$OPTARG;;
+        \?) echo "Invalid option -$OPTARG" >&2;;
+    esac
 done
+    
+if [[ ! -e $RCHOME ]]; then
+    git clone https://github.com/whlin/rc $RCHOME
+fi
+
+echo "----- CLEAN -----"
+cleanRCFiles
+
+if [[ "${CLEAN}" != true ]]; then
+    echo "----- BUILD -----"
+    buildRCFiles
+fi
 
